46317de82b791752e48db93467d54d3206aa312b
saharsh <saharshdc@gmail.com>
2022-12-10 14:23:20 -0500

    fixed typo and contact spacing on mobile

--- src/components/About.js
+++ src/components/About.js
@@ -11,12 +11,12 @@ export default function Header() {
       <a href="#contact"><img src={headshot} className = 'rounded headshot m-4 shadow ' alt="headshot" height="200px"/></a>
       <div className=''>
 
-      <h1 className=''>Hi! I'm Saharsh! (but you can call me sash)</h1>
+      <h1 className=''>Hi! I'm Saharsh! (but you can call me Sash)</h1>
       <p>
         I'm a full stack developer with a background in photography, advertising, and graphic design. I aim to use my years in the visual arts to create webapps that are bold and pleasing to the eye, and a joy to use. 
       </p>
       <p>
-        With over 12 years of work in client facing fields all over the world, I am well versed in making things that look good, feel good, and making my customers happy. 
+        With over 12 years of work in client facing fields all over the world, I am well versed in making things that look good, feel good, and make my clients happy. 
       </p>
       <p>
         For the 5 minutes a day that I'm not writing code or editing photos, I'm probably incurring the wrath of my 2 kids, Zoe & Ozzy.
