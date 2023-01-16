57b855048d6c87943690aefeb422e916a7b86df3
saharsh <saharshdc@gmail.com>
2023-01-16 11:52:40 -0500

    added CV

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,20 @@ import React from "react";
 import headshotpx from "../assets/images/headshot.jpg";
 import headshot from "../assets/images/headshotpx.jpg";
 import "animate.css";
+import Resume from "../assets/files/Saharsh-Cherian-CV.pdf";
 
 export default function Contact() {
   return (
-    <div className=" section text-right" >
-              <div className="parallax3 parallax animate__animated animate__fadeIn "> <h1 className="title p-3 ">CONTACT ME </h1> </div>
+    <div className=" section text-right">
+      <div className="parallax3 parallax animate__animated animate__fadeIn ">
+        {" "}
+        <h1 className="title p-3 ">CONTACT ME </h1>{" "}
+      </div>
       <div
         className="d-flex profile contact  w-50 m-auto animate__animated animate__fadeInUp "
         id="contact">
-  
-        <div >
-          <h2 >
-            Get in touch, ask me something, tell me something...
-          </h2>
+        <div>
+          <h2>Get in touch, ask me something, tell me something...</h2>
           <ul className="contact list-unstyled  ">
             <li>
               <a href="mailto:saharshdc@gmail.com" className="nav-link pl-0">
@@ -26,22 +27,37 @@ export default function Contact() {
                 <i className="fa fa-phone"></i> Phone
               </a>
             </li>
+
+            <li>
+              <a href={Resume} className="nav-link pl-0" target="blank">
+                <i className="fa fa-file"></i> View My CV{" "}
+              </a>
+            </li>
+            <li>
+              <a
+                href={Resume}
+                download="Saharsh Cherian - Full Stack WebDev"
+                className="nav-link pl-0">
+                <i className="fa fa-solid fa-download"></i> Download My CV{" "}
+              </a>
+            </li>
           </ul>
         </div>
         <div className="headshots mt-2">
-        <a href="#about">
-        <img
-            src={headshot}
-            className="rounded headshot mx-4 shadow "
-            alt="headshot"
-            height="200px"
-          />
-        <img
-          src={headshotpx}
-          className="rounded headshotpx mx-4 shadow "
-          alt="headshot"
-          height="200px"
-        /></a>
+          <a href="#about">
+            <img
+              src={headshot}
+              className="rounded headshot mx-4 shadow "
+              alt="headshot"
+              height="200px"
+            />
+            <img
+              src={headshotpx}
+              className="rounded headshotpx mx-4 shadow "
+              alt="headshot"
+              height="200px"
+            />
+          </a>
         </div>
       </div>
     </div>
