a62d3851df6f9db8e50ea3eea7589662990954f6
Saharsh Cherian <saharshdc@gmail.com>
2023-02-21 20:46:10 -0500

    testing google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Main from './components/Main';
 import './App.css';
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-Y97Z1XJD6V"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 const App = () => <Main className="hidden-section"/>;
 
 export default App;
